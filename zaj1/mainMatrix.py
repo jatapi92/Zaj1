f8cc03190e549bf0e9c7beb38da87ebedff8858a
Jakub P <serwisowekonto99@gmail.com>
2025-03-16 10:32:19 +0100

    add localization

--- zaj1/mainMatrix.py
+++ zaj1/mainMatrix.py
@@ -1,7 +1,7 @@
 import matrix
 
-matrix1 = matrix.Matrix(1,2,3,4)
-matrix2 = matrix.Matrix(2,0,1,2)
+matrix1 = matrix.Matrix(1, 2, 3, 4)
+matrix2 = matrix.Matrix(2, 0, 1, 2)
 matrix3 = matrix1 + matrix2
 print(matrix3)
 print(repr(matrix3))
