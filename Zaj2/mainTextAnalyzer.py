0911a89f8ed5badc571f35d0a4c28083329eff63
Jakub P <serwisowekonto99@gmail.com>
2025-03-23 10:16:34 +0100

    main additional examples of statements

--- Zaj2/mainTextAnalyzer.py
+++ Zaj2/mainTextAnalyzer.py
@@ -1,4 +1,8 @@
 import textAnalyzer
 
 zdanie1 = textAnalyzer.AdvancedTextAnalyzer('Mam dobry dzien! Co za dzien')
+zdanie2 = textAnalyzer.AdvancedTextAnalyzer('Mam zły dzien! Co za dzien')
+zdanie3 = textAnalyzer.AdvancedTextAnalyzer('Mam dzien! Co za dzien')
 print(f'Ocena zdania:"{zdanie1.show_text()}" - {zdanie1.sentiment_analysis()}')
+print(f'Ocena zdania:"{zdanie2.show_text()}" - {zdanie2.sentiment_analysis()}')
+print(f'Ocena zdania:"{zdanie3.show_text()}" - {zdanie3.sentiment_analysis()}')
